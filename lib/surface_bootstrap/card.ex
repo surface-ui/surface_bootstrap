f47e98b1a6f044f72961ae5686793b7b227a4f50
Oliver Mulelid-Tynes <oliver@omt.tech>
2021-03-17 14:34:36 +0100

    add cards

--- lib/surface_bootstrap/card.ex
+++ lib/surface_bootstrap/card.ex
@@ -1,6 +1,14 @@
 defmodule SurfaceBootstrap.Card do
   @moduledoc """
-  Card component
+  Card component.
+
+  Can hold:
+  - Card.Body
+    - Card.Body.Title (inside Body)
+  - Card.Footer
+  - List groups (https://getbootstrap.com/docs/5.0/components/list-group/)
+  - `<a class="card-link">` (Link, LiveRedirect, LivePatch)
+  - `<img class="card-img-top">` and `<img class="card-img-bottom">`
 
   https://getbootstrap.com/docs/5.0/components/card/
   """
@@ -8,6 +16,10 @@ defmodule SurfaceBootstrap.Card do
 
   prop text_align, :string, values: ~w(left center right)
 
+  prop width, :string, values: ~w(25 50 75 100)
+
+  slot card_header
+  slot card_footer
   slot default
 
   def render(assigns) do
@@ -15,11 +27,30 @@ defmodule SurfaceBootstrap.Card do
     <div class={{
       "card",
       "text-center": @text_align == "center",
-      "text-end": @text_align == "end"
-
+      "text-end": @text_align == "end",
+      "w-#{@width}": @width
     }}>
+      <div :if={{slot_assigned?(:card_header)}} class="card-header">
+      <slot name="card_header"/>
+      </div>
       <slot />
+
+      <div :if={{slot_assigned?(:card_footer)}} class={{["card-footer"] ++ footer_class(@card_footer) }}>
+      <slot name="card_footer"/>
+      </div>
     </div>
     """
+  end
+
+  defp footer_class(footer) do
+    IO.inspect(footer)
+
+    case footer do
+      [%{class: class}] ->
+        class
+
+      _ ->
+        []
+    end
   end
 end
