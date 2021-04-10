f940846de097bed5ed64fa619533c35b8b61d705
Jørgen Orehøj Erichsen <joe@erichsen.net>
2021-04-10 16:58:45 +0200

    Use the newest bootstrap@5.0.0-beta3 version

--- priv/catalogue/catalogue.ex
+++ priv/catalogue/catalogue.ex
@@ -15,7 +15,7 @@ defmodule SurfaceBootstrap.Catalogue do
   def config() do
     [
       head_css: """
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
       <style>#{@app_css}</style>
       """,
